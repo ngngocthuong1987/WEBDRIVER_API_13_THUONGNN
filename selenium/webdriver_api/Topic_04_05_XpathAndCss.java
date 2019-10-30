6775746ae6608d6f284838548a95fefd60425726
Thuong Nguyen <ngngocthuong1987@gmail.com>
2019-10-30 21:50:21 +0700

    Change to Chrome

--- selenium/webdriver_api/Topic_04_05_XpathAndCss.java
+++ selenium/webdriver_api/Topic_04_05_XpathAndCss.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -48,7 +48,8 @@ public class Topic_04_05_XpathAndCss {
 
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "libraries/chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
