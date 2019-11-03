0a089a4a65f093b14ce8c2bbcaa4b81eb4729ef4
Thuong Nguyen <ngngocthuong1987@gmail.com>
2019-11-03 15:55:12 +0700

    Update source code

--- selenium/webdriver_api/Topic_04_05_XpathAndCss.java
+++ selenium/webdriver_api/Topic_04_05_XpathAndCss.java
@@ -11,21 +11,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Topic_04_05_XpathAndCss {
 
-	// Message constant
+	// CONSTANT: Message expected
 	static final String REQUIRED_ERROR_MESSAGE = "This is a required field.";
 	static final String EMAIL_ERROR_MESSAGE = "Please enter a valid email address. For example johndoe@domain.com.";
 	static final String SHORT_PASSWORD_ERROR_MESSAGE = "Please enter 6 or more characters without leading or trailing spaces.";
 	static final String LOGIN_ERROR_MESSAGE = "Invalid login or password.";
 	static final String REGISTER_SUCCESS_MESSAGE = "Thank you for registering with Main Website Store.";
+
+	// CONSTANT: Website elements
+	static final String WEBSITE_URL = "http://live.demoguru99.com/";
 	static final String HOME_PAGE_TITLE = "Home page";
 
-	// XPath of link, button
+	// CONSTANT: XPath of link, button
 	static final String XPATH_ACCOUNT_LINK = "//div[@class='account-cart-wrapper']//span[text()='Account']";
-	static final String XPATH_MY_ACCOUNT_LINK = "//div[@id='header-account']//a[@title='My Account']";
+	static final String XPATH_MY_ACCOUNT_LINK = "//div[@class='footer']//a[@title='My Account']";
 	static final String XPATH_LOGIN_BUTTON = "//button[@id='send2']";
 	static final String XPATH_INPUT_EMAIL_LOGIN = "//input[@id='email']";
 	static final String XPATH_INPUT_PASSWORD_LOGIN = "//input[@id='pass']";
@@ -38,7 +42,7 @@ public class Topic_04_05_XpathAndCss {
 	static final String XPATH_REGISTER_BUTTON = "//button[@title='Register']";
 	static final String XPATH_LOGOUT_LINK = "//a[@title='Log Out']";
 
-	// XPath of message
+	// CONSTANT: XPath of message validate
 	static final String XPATH_REQUIRED_EMAIL_MESSAGE = "//div[@id='advice-required-entry-email']";
 	static final String XPATH_REQUIRED_PASS_MESSAGE = "//div[@id='advice-required-entry-pass']";
 	static final String XPATH_EMAIL_INVALID_MESSAGE = "//div[@id='advice-validate-email-email']";
@@ -46,64 +50,73 @@ public class Topic_04_05_XpathAndCss {
 	static final String XPATH_INVALID_LOGIN_MESSAGE = "//li[@class='error-msg']";
 	static final String XPATH_REGISTER_SUCCESS_MESSAGE = "//li[@class='success-msg']";
 
-	static final String WEBSITE_URL = "http://live.demoguru99.com/";
+	// CONSTANT: Xpath of login information
+	static final String MY_DASHBOARD = "//h1[text()='My Dashboard']";
+	static final String HELLO_MESSAGE = "//strong[text()='Hello, Automation Testing!']";
+	static final String CONTACT_INFO = "//div[@class='box-content']//p[contains(text(),'Automation Testing')]";
+
 	WebDriver driver;
 
 	@BeforeClass
-	public void beforeClass() {
+	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "libraries/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
+	@BeforeMethod
+	public void goToWebAndClickMyAccountLink() {
+		driver.get(WEBSITE_URL);
+		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
+	}
+
 	@Test
 	public void TC_01_LoginEmpty() {
-		driver.get(WEBSITE_URL);
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
 		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(REQUIRED_ERROR_MESSAGE, driver.findElement(By.xpath(XPATH_REQUIRED_EMAIL_MESSAGE)).getText());
-		Assert.assertEquals(REQUIRED_ERROR_MESSAGE, driver.findElement(By.xpath(XPATH_REQUIRED_PASS_MESSAGE)).getText());
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REQUIRED_EMAIL_MESSAGE)).getText(), REQUIRED_ERROR_MESSAGE);
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REQUIRED_PASS_MESSAGE)).getText(), REQUIRED_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_02_LoginWithEmailInvalid() {
-		driver.get(WEBSITE_URL);
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
 		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("123434234@12312.123123");
 		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(EMAIL_ERROR_MESSAGE, driver.findElement(By.xpath(XPATH_EMAIL_INVALID_MESSAGE)).getText());
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_EMAIL_INVALID_MESSAGE)).getText(), EMAIL_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_03_LoginWithShortPassword() {
-		driver.get(WEBSITE_URL);
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
 		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation@gmail.com");
 		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123");
 		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(SHORT_PASSWORD_ERROR_MESSAGE, driver.findElement(By.xpath(XPATH_SHORT_PASSWORD_MESSAGE)).getText());
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_SHORT_PASSWORD_MESSAGE)).getText(), SHORT_PASSWORD_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_04_LoginWithPasswordIncorrect() {
-		driver.get(WEBSITE_URL);
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
 		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation@gmail.com");
 		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123123123");
 		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(LOGIN_ERROR_MESSAGE, driver.findElement(By.xpath(XPATH_INVALID_LOGIN_MESSAGE)).getText());
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_INVALID_LOGIN_MESSAGE)).getText(), LOGIN_ERROR_MESSAGE);
 	}
 
 	@Test
-	public void TC_05_CreateAnAccount() {
-		driver.get(WEBSITE_URL);
+	public void TC_05_LoginWithEmailValid() {
+		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation_13@gmail.com");
+		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123123");
+		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
+		Assert.assertTrue(driver.findElement(By.xpath(MY_DASHBOARD)).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath(HELLO_MESSAGE)).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath(CONTACT_INFO)).isDisplayed());
+
+		//If not logout in test case 5, when click to My Account link will redirect to detail account page In test case 6
 		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
+		driver.findElement(By.xpath(XPATH_LOGOUT_LINK)).click();
+	}
+
+	@Test
+	public void TC_06_CreateAnAccount() {
 		driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT_BUTTON)).click();
 		driver.findElement(By.xpath(XPATH_INPUT_FIRST_NAME)).sendKeys("THUONG");
 		driver.findElement(By.xpath(XPATH_INPUT_LAST_NAME)).sendKeys("NGUYEN");
@@ -111,7 +124,7 @@ public class Topic_04_05_XpathAndCss {
 		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD)).sendKeys("abc123456");
 		driver.findElement(By.xpath(XPATH_INPUT_CONFIRM_PASSWORD)).sendKeys("abc123456");
 		driver.findElement(By.xpath(XPATH_REGISTER_BUTTON)).click();
-		Assert.assertEquals(REGISTER_SUCCESS_MESSAGE, driver.findElement(By.xpath(XPATH_REGISTER_SUCCESS_MESSAGE)).getText());
+		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REGISTER_SUCCESS_MESSAGE)).getText(), REGISTER_SUCCESS_MESSAGE);
 		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
 		driver.findElement(By.xpath(XPATH_LOGOUT_LINK)).click();
 
@@ -122,7 +135,7 @@ public class Topic_04_05_XpathAndCss {
 	}
 
 	@AfterClass
-	public void afterClass() {
+	public void terminateBrowser() {
 		driver.quit();
 	}
 
