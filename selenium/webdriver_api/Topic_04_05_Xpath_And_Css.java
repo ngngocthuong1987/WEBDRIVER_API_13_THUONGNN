27c7191e0b8a6e27cdf78d67aad27bda30f1261e
Thuong Nguyen <ngngocthuong1987@gmail.com>
2019-11-09 17:54:04 +0700

    Update Topic 04 & 05

--- selenium/webdriver_api/Topic_04_05_Xpath_And_Css.java
+++ selenium/webdriver_api/Topic_04_05_Xpath_And_Css.java
@@ -16,6 +16,17 @@ import org.testng.annotations.Test;
 
 public class Topic_04_05_Xpath_And_Css {
 
+	// Setting varriables
+	WebDriver driver;
+	String invalidEmail = "123434234@12312.123123";
+	String validEmail = "automation@gmail.com";
+	String shortPassword = "123";
+	String invalidPassword = "123123123";
+	String firstName = "THUONG";
+	String lastName = "NGUYEN";
+	String email = getRandomEmail();
+	String validPassword = "123456789";
+
 	// CONSTANT: Message expected
 	static final String REQUIRED_ERROR_MESSAGE = "This is a required field.";
 	static final String EMAIL_ERROR_MESSAGE = "Please enter a valid email address. For example johndoe@domain.com.";
@@ -27,35 +38,30 @@ public class Topic_04_05_Xpath_And_Css {
 	static final String WEBSITE_URL = "http://live.demoguru99.com/";
 	static final String HOME_PAGE_TITLE = "Home page";
 
-	// CONSTANT: XPath of link, button
-	static final String XPATH_ACCOUNT_LINK = "//div[@class='account-cart-wrapper']//span[text()='Account']";
-	static final String XPATH_MY_ACCOUNT_LINK = "//div[@class='footer']//a[@title='My Account']";
-	static final String XPATH_LOGIN_BUTTON = "//button[@id='send2']";
-	static final String XPATH_INPUT_EMAIL_LOGIN = "//input[@id='email']";
-	static final String XPATH_INPUT_PASSWORD_LOGIN = "//input[@id='pass']";
-	static final String XPATH_CREATE_ACCOUNT_BUTTON = "//a[@title='Create an Account']";
-	static final String XPATH_INPUT_FIRST_NAME = "//input[@id='firstname']";
-	static final String XPATH_INPUT_LAST_NAME = "//input[@id='lastname']";
-	static final String XPATH_INPUT_EMAIL = "//input[@id='email_address']";
-	static final String XPATH_INPUT_PASSWORD = "//input[@id='password']";
-	static final String XPATH_INPUT_CONFIRM_PASSWORD = "//input[@id='confirmation']";
-	static final String XPATH_REGISTER_BUTTON = "//button[@title='Register']";
-	static final String XPATH_LOGOUT_LINK = "//a[@title='Log Out']";
-
-	// CONSTANT: XPath of message validate
-	static final String XPATH_REQUIRED_EMAIL_MESSAGE = "//div[@id='advice-required-entry-email']";
-	static final String XPATH_REQUIRED_PASS_MESSAGE = "//div[@id='advice-required-entry-pass']";
-	static final String XPATH_EMAIL_INVALID_MESSAGE = "//div[@id='advice-validate-email-email']";
-	static final String XPATH_SHORT_PASSWORD_MESSAGE = "//div[@id='advice-validate-password-pass']";
-	static final String XPATH_INVALID_LOGIN_MESSAGE = "//li[@class='error-msg']";
-	static final String XPATH_REGISTER_SUCCESS_MESSAGE = "//li[@class='success-msg']";
-
-	// CONSTANT: Xpath of login information
-	static final String MY_DASHBOARD = "//h1[text()='My Dashboard']";
-	static final String HELLO_MESSAGE = "//strong[text()='Hello, Automation Testing!']";
-	static final String CONTACT_INFO = "//div[@class='box-content']//p[contains(text(),'Automation Testing')]";
-
-	WebDriver driver;
+	// Setting some By object of elements xpath
+	By accountLink = By.xpath("//div[@class='account-cart-wrapper']//span[text()='Account']");
+	By myAccountLink = By.xpath("//div[@class='footer']//a[@title='My Account']");
+	By loginButton = By.xpath("//button[@id='send2']");
+	By emailLoginInput = By.xpath("//input[@id='email']");
+	By passwordLoginInput = By.xpath("//input[@id='pass']");
+	By createAccountButton = By.xpath("//a[@title='Create an Account']");
+	By firstNameInput = By.xpath("//input[@id='firstname']");
+	By lastNameInput = By.xpath("//input[@id='lastname']");
+	By emailInput = By.xpath("//input[@id='email_address']");
+	By passWordInput = By.xpath("//input[@id='password']");
+	By passWordConfirmInput = By.xpath("//input[@id='confirmation']");
+	By registerButton = By.xpath("//button[@title='Register']");
+	By logoutLink = By.xpath("//a[@title='Log Out']");
+	By requiredEmailMessage = By.xpath("//div[@id='advice-required-entry-email']");
+	By requiredPasswordMessage = By.xpath("//div[@id='advice-required-entry-pass']");
+	By invalidEmailMessage = By.xpath("//div[@id='advice-validate-email-email']");
+	By shortPasswordMessage = By.xpath("//div[@id='advice-validate-password-pass']");
+	By invalidLoginMessage = By.xpath("//li[@class='error-msg']");
+	By registerSuccessMessage = By.xpath("//li[@class='success-msg']");
+	By myDashboard = By.xpath("//h1[text()='My Dashboard']");
+	By helloMessage = By.xpath("//strong[text()='Hello, " + firstName + " " + lastName + "!']");
+	By contactInfor = By.xpath("//div[@class='box-content']//p[contains(text(),'"+ firstName + " " + lastName+"')]");
+	By emailInfor = By.xpath("//div[@class='box-content']//p[contains(text(),'"+ email +"')]");
 
 	@BeforeClass
 	public void launchBrowser() {
@@ -68,70 +74,66 @@ public class Topic_04_05_Xpath_And_Css {
 	@BeforeMethod
 	public void goToWebAndClickMyAccountLink() {
 		driver.get(WEBSITE_URL);
-		driver.findElement(By.xpath(XPATH_MY_ACCOUNT_LINK)).click();
+		driver.findElement(myAccountLink).click();
 	}
 
 	@Test
 	public void TC_01_LoginEmpty() {
-		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REQUIRED_EMAIL_MESSAGE)).getText(), REQUIRED_ERROR_MESSAGE);
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REQUIRED_PASS_MESSAGE)).getText(), REQUIRED_ERROR_MESSAGE);
+		driver.findElement(loginButton).click();
+		Assert.assertEquals(driver.findElement(requiredEmailMessage).getText(), REQUIRED_ERROR_MESSAGE);
+		Assert.assertEquals(driver.findElement(requiredPasswordMessage).getText(), REQUIRED_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_02_LoginWithEmailInvalid() {
-		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("123434234@12312.123123");
-		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_EMAIL_INVALID_MESSAGE)).getText(), EMAIL_ERROR_MESSAGE);
+		driver.findElement(emailLoginInput).sendKeys(invalidEmail);
+		driver.findElement(loginButton).click();
+		Assert.assertEquals(driver.findElement(invalidEmailMessage).getText(), EMAIL_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_03_LoginWithShortPassword() {
-		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation@gmail.com");
-		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123");
-		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_SHORT_PASSWORD_MESSAGE)).getText(), SHORT_PASSWORD_ERROR_MESSAGE);
+		driver.findElement(emailLoginInput).sendKeys(validEmail);
+		driver.findElement(passwordLoginInput).sendKeys(shortPassword);
+		driver.findElement(loginButton).click();
+		Assert.assertEquals(driver.findElement(shortPasswordMessage).getText(), SHORT_PASSWORD_ERROR_MESSAGE);
 	}
 
 	@Test
 	public void TC_04_LoginWithPasswordIncorrect() {
-		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation@gmail.com");
-		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123123123");
-		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_INVALID_LOGIN_MESSAGE)).getText(), LOGIN_ERROR_MESSAGE);
+		driver.findElement(emailLoginInput).sendKeys(validEmail);
+		driver.findElement(passwordLoginInput).sendKeys(invalidPassword);
+		driver.findElement(loginButton).click();
+		Assert.assertEquals(driver.findElement(invalidLoginMessage).getText(), LOGIN_ERROR_MESSAGE);
 	}
 
 	@Test
-	public void TC_05_LoginWithEmailValid() {
-		driver.findElement(By.xpath(XPATH_INPUT_EMAIL_LOGIN)).sendKeys("automation_13@gmail.com");
-		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD_LOGIN)).sendKeys("123123");
-		driver.findElement(By.xpath(XPATH_LOGIN_BUTTON)).click();
-		Assert.assertTrue(driver.findElement(By.xpath(MY_DASHBOARD)).isDisplayed());
-		Assert.assertTrue(driver.findElement(By.xpath(HELLO_MESSAGE)).isDisplayed());
-		Assert.assertTrue(driver.findElement(By.xpath(CONTACT_INFO)).isDisplayed());
-
-		//If not logout in test case 5, when click to My Account link will redirect to detail account page In test case 6
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_LOGOUT_LINK)).click();
-	}
-
-	@Test
-	public void TC_06_CreateAnAccount() {
-		driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT_BUTTON)).click();
-		driver.findElement(By.xpath(XPATH_INPUT_FIRST_NAME)).sendKeys("THUONG");
-		driver.findElement(By.xpath(XPATH_INPUT_LAST_NAME)).sendKeys("NGUYEN");
-		driver.findElement(By.xpath(XPATH_INPUT_EMAIL)).sendKeys(getRandomEmail());
-		driver.findElement(By.xpath(XPATH_INPUT_PASSWORD)).sendKeys("abc123456");
-		driver.findElement(By.xpath(XPATH_INPUT_CONFIRM_PASSWORD)).sendKeys("abc123456");
-		driver.findElement(By.xpath(XPATH_REGISTER_BUTTON)).click();
-		Assert.assertEquals(driver.findElement(By.xpath(XPATH_REGISTER_SUCCESS_MESSAGE)).getText(), REGISTER_SUCCESS_MESSAGE);
-		driver.findElement(By.xpath(XPATH_ACCOUNT_LINK)).click();
-		driver.findElement(By.xpath(XPATH_LOGOUT_LINK)).click();
+	public void TC_05_CreateAnAccount() {
+		driver.findElement(createAccountButton).click();
+		driver.findElement(firstNameInput).sendKeys(firstName);
+		driver.findElement(lastNameInput).sendKeys(lastName);
+		driver.findElement(emailInput).sendKeys(email);
+		driver.findElement(passWordInput).sendKeys(validPassword);
+		driver.findElement(passWordConfirmInput).sendKeys(validPassword);
+		driver.findElement(registerButton).click();
+		Assert.assertEquals(driver.findElement(registerSuccessMessage).getText(), REGISTER_SUCCESS_MESSAGE);
+		driver.findElement(accountLink).click();
+		driver.findElement(logoutLink).click();
 
 		// Wait browser redirect 20 seconds
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		wait.until(ExpectedConditions.titleIs(HOME_PAGE_TITLE));
 		Assert.assertEquals(HOME_PAGE_TITLE, driver.getTitle());
+	}
+
+	@Test
+	public void TC_06_LoginWithEmailValid() {
+		driver.findElement(emailLoginInput).sendKeys(email);
+		driver.findElement(passwordLoginInput).sendKeys(validPassword);
+		driver.findElement(loginButton).click();
+		Assert.assertTrue(driver.findElement(myDashboard).isDisplayed());
+		Assert.assertTrue(driver.findElement(helloMessage).isDisplayed());
+		Assert.assertTrue(driver.findElement(contactInfor).isDisplayed());
 	}
 
 	@AfterClass
