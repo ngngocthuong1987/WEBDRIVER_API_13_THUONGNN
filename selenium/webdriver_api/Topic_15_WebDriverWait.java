ffe011bf7281da5d80a1202cd3b9e29e0697fc84
Thuong Nguyen <ngngocthuong1987@gmail.com>
2019-12-23 22:37:34 +0700

    Update test case

--- selenium/webdriver_api/Topic_15_WebDriverWait.java
+++ selenium/webdriver_api/Topic_15_WebDriverWait.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -26,13 +27,21 @@ public class Topic_15_WebDriverWait {
 	By messageErrorBy = By.xpath("//div[@class='alert alert-danger']");
 	By messageErrorNotInDOMBy = By.xpath("//div[@class='alert alert-danger' and not (@id='create_account_error')]");
 
+	By startButton = By.xpath("//button[text()='Start']");
+	By resultText = By.xpath("//div[@id='finish']/h4[text()='Hello World!']");
+	By loadingIcon = By.xpath("//div[@id='loading']");
+
+	By daySelect = By.xpath("//a[text()='26']");
+	By daySelected = By.xpath("//a[text()='26']/parent::td[@class='rcSelected']");
+	By textDaySelected = By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']");
+	By loadingAjaxIcon = By.xpath("//div[@class='raDiv']");
+
 	@BeforeClass
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "libraries/chromedriver.exe");
 		driver = new ChromeDriver();
-		explicitWait = new WebDriverWait(driver, 10);
 
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
@@ -71,7 +80,8 @@ public class Topic_15_WebDriverWait {
 	}
 
 	@Test
-	public void TC_O1_Presence() {
+	public void TC_01_Presence() {
+		explicitWait = new WebDriverWait(driver, 10);
 		driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
 
 		//Pass - Element displayed in UI and have in DOM
@@ -82,6 +92,79 @@ public class Topic_15_WebDriverWait {
 
 		//Fail - Element do not displayed in UI and do not have in DOM
 		explicitWait.until(ExpectedConditions.presenceOfElementLocated(messageErrorNotInDOMBy));
+	}
+
+	@Test
+	public void TC_02_ImplicitWait_Fail() {
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_02_ImplicitWait_Pass() {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_03_ExplicitWait_Invisible_03_Seconds() {
+		explicitWait = new WebDriverWait(driver, 3);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(loadingIcon));
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_03_ExplicitWait_Invisible_05_Seconds() {
+		explicitWait = new WebDriverWait(driver, 5);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(loadingIcon));
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_04_ExplicitWait_Visible_03_Seconds() {
+		explicitWait = new WebDriverWait(driver, 3);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(resultText));
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_04_ExplicitWait_Visible_05_Seconds() {
+		explicitWait = new WebDriverWait(driver, 1);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+		driver.findElement(startButton).click();
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(resultText));
+		Assert.assertTrue(driver.findElement(resultText).isDisplayed());
+	}
+
+	@Test
+	public void TC_05_ExplicitWait() {
+		explicitWait = new WebDriverWait(driver, 10);
+		driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+		Assert.assertEquals(driver.findElement(textDaySelected).getText(), "No Selected Dates to display.");
+
+		driver.findElement(daySelect).click();
+		Assert.assertTrue(driver.findElement(daySelected).isDisplayed());
+
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(loadingAjaxIcon));
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(daySelected));
+		Assert.assertEquals(driver.findElement(textDaySelected).getText(), "Thursday, December 26, 2019");
+
 	}
 
 	@AfterClass
